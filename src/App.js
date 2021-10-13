6f94b362ed8839ae50cae67f0659565ffabcb6c2
Natasha <natasha.pinder00@gmail.com>
2021-10-13 11:49:08 +0100

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   <h1> Weather App </h1>
 
 <footer> 
-  This project was coded by <a href="https://www.linkedin.com/in/natasha-p-9a3621173/" target="_blank">Natasha Pinder</a>  and is{" "}
+  This project was coded by <a href="https://www.linkedin.com/in/natasha-p-9a3621173/" target="_blank" rel="noreferrer">Natasha Pinder</a>  and is{" "}
   <a href="https://github.com/natashapinder00/updated-react-weather-app" 
-  target="_blank"> 
+  target="_blank" rel="noreferrer"> 
   open-sourced on Github
   </a>
   </footer>
