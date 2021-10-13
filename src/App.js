9ae7cb56c3670d6ae8e592b8d576f6cc4710d2bf
Natasha <natasha.pinder00@gmail.com>
2021-10-13 13:42:30 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="contrainer">
   <h1> Weather App </h1>
 
 <footer> 
-  This project was coded by <a href="https://www.linkedin.com/in/natasha-p-9a3621173/" target="_blank" rel="noreferrer">Natasha Pinder</a>  and is{" "}
+  This project was coded by <a href="https://www.linkedin.com/in/natasha-p-9a3621173/" 
+  target="_blank" rel="noreferrer">Natasha Pinder</a>  and is{" "}
   <a href="https://github.com/natashapinder00/updated-react-weather-app" 
   target="_blank" rel="noreferrer"> 
   open-sourced on Github
   </a>
   </footer>
+  </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import './index.css';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
