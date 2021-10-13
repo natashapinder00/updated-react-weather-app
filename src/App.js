a4b7c03e9a1ed636fd5c7db3d4fc72cd3f817ef3
Natasha <natasha.pinder00@gmail.com>
2021-10-13 11:40:10 +0100

    linked my github and linkedin

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  Weather App
+  <h1> Weather App </h1>
+
+<footer> 
+  This project was coded by <a href="https://www.linkedin.com/in/natasha-p-9a3621173/" target="_blank">Natasha Pinder</a>  and is{" "}
+  <a href="https://github.com/natashapinder00/updated-react-weather-app" 
+  target="_blank"> 
+  open-sourced on Github
+  </a>
+  </footer>
     </div>
   );
 }
