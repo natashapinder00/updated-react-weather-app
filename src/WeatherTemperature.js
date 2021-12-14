055281a8a0557823d80f9ff47f59e4727956d8fc
Natasha <natasha.pinder00@gmail.com>
2021-12-14 19:14:17 +0000

    created new componant, displayed the relevant temp

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,15 +4,14 @@ export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
   function convertToFahrenheit(event) {
     event.preventDefault();
-    setUnit("Fahrenheit");
+    setUnit("fahrenheit");
 
   }
-
-  function showcelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
+function convertToCelsius(event) {  
+  event.preventDefault();
+  setUnit("celsius");
+}
+ 
   if (unit === "celsius") {
   return (
     <div className="WeatherTemperature">
@@ -20,12 +19,27 @@ export default function WeatherTemperature(props) {
       </span>
       <span className="unit">
            °C |{" "}
-        <a href="/" onClick={showFahrenheit}>
+        <a href="/" onClick={convertToFahrenheit}>
         °F
         </a>
          </span>
          </div>
   );
 
+  } else {  
+    let fahrenheit = (props.celsius * 9/5) + 32;
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(fahrenheit)}
+      </span>
+      <span className="unit">
+        <a href="/" onClick={convertToCelsius}>
+           °C </a>|{" "}
+        °F
+      
+         </span>
+         </div>
+  );
+ 
   }
 }
